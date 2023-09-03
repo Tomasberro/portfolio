204f28d362d8c86857fd5013f11d53a5cae6bcee
Tomasberro <80068868+Tomasberro@users.noreply.github.com>
2023-09-03 19:52:21 -0300

    finish

--- src/components/WebWorks/WebWorks.jsx
+++ src/components/WebWorks/WebWorks.jsx
@@ -4,9 +4,9 @@ import img2 from '../../assets/web/guamen.png';
 import img3 from '../../assets/web/Ten.png';
 import img4 from '../../assets/web/buro.png';
 import img5 from '../../assets/web/yutex.jpg';
-import img6 from '../../assets/web/tarjetaaby.png';
-import img8 from '../../assets/web/clima.png';
-import img9 from '../../assets/web/movies.png';
+import img6 from '../../assets/web/wove.png';
+import img8 from '../../assets/web/pokemon.png';
+import img9 from '../../assets/web/cinema.png';
 import img10 from '../../assets/web/countries.png';
 import img12 from '../../assets/web/drops.png';
 import './WebWorks.css';
@@ -35,66 +35,12 @@ function WebWorks() {
                 <div className="webWorksData">
 
                     {/* -------------------------------------------------------------------------- */}
-                 
-                    {/* -------------------------------------------------------------------------- */}
-               
-                    {/* -------------------------------------------------------------------------- */}
-                    <div className="card" >
-                        <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(3) : ''}>
-                            <div className="card__face card__face--front">
-                                <h2>BURÓ DIGITAL</h2>
-                                <div className={ intFrameWidthWeb < 800 ? "tooltipTitle":"invisible"}>
-                                    da click para ver la información
-                                </div>
-                            </div>
-                            <div className="card__face card__face--back" >
-                                <div className="card__content" >
-                                     <div className="card__header">
-                                        <img src={img4} alt="Trabajos Web" className="pp" />
-                                    </div>
-                                    <div className="card__body">
-                                        <h4>Negocio</h4>
-                                        <p>Establecimiento que brinda servicios digitales, 
-                                           ubicada en la ciudad de León, Guanajuato. 
-                                        </p>
-                                        {/* <a className="LinkWebSite" href="#" target="_blank">visitar</a> */}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> 
-                    {/* -------------------------------------------------------------------------- */}
-                    <div className="card" >
-                        <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(4) : ''}>
-                            <div className="card__face card__face--front">
-                                <h2>YUTEX</h2>
-                                <div className={ intFrameWidthWeb < 800 ? "tooltipTitle":"invisible"}>
-                                    da click para ver la información
-                                </div>
-                            </div>
-                            <div className="card__face card__face--back" >
-                                <div className="card__content" >
-                                     <div className="card__header">
-                                        <img src={img5} alt="Trabajos Web" className="pp" />
-                                    </div>
-                                    <div className="card__body">
-                                        <h4>Empresa</h4>
-                                        <p>Venta de insumos para fabricación de calzado, 
-                                           ubicada en San Francisco del Rincón, Guanajuato. 
-                                        </p>
-                                        {/* <a className="LinkWebSite" href="#" target="_blank">visitar</a> */}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> 
-                    {/* -------------------------------------------------------------------------- */}
                     <div className="card" >
                         <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(5) : ''}>
                             <div className="card__face card__face--front">
-                                <h2>TARJETA DIGITAL</h2>
+                                <h2>Wovemade</h2>
                                 <div className={ intFrameWidthWeb < 800 ? "tooltipTitle":"invisible"}>
-                                    da click para ver la información
+                                click to see the information
                                 </div>
                             </div>
                             <div className="card__face card__face--back" >
@@ -103,11 +49,10 @@ function WebWorks() {
                                         <img src={img6} alt="Trabajos Web" className="ppCard" />
                                     </div>
                                     <div className="card__body">
-                                        <h4>Consultora</h4>
-                                        <p>Tarjeta de contacto digital para consultora de belleza, 
-                                           solo versión para smartphone. 
+                                        <h4>Ring store</h4>
+                                        <p>Web app for the ring sales company.
                                         </p>
-                                        {/* <a className="LinkWebSite" href="#" target="_blank">visitar</a> */}
+                                        <a className="LinkWebSite" href="https://wovemade.com/" target="_blank">Go</a> 
                                     </div>
                                 </div>
                             </div>
@@ -115,24 +60,23 @@ function WebWorks() {
                     </div> 
                     {/* -------------------------------------------------------------------------- */}
                     <div className="card" >
-                        <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(6) : ''}>
+                        <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(9) : ''}>
                             <div className="card__face card__face--front">
-                                <h2>WEATHER APP</h2>
+                                <h2>Drops APP</h2>
                                 <div className={ intFrameWidthWeb < 800 ? "tooltipTitle":"invisible"}>
-                                    da click para ver la información
+                                click to see the information
                                 </div>
                             </div>
                             <div className="card__face card__face--back" >
                                 <div className="card__content" >
                                      <div className="card__header">
-                                        <img src={img8} alt="Trabajos Web" className="pp" />
+                                        <img src={img12} alt="Trabajos Web" className="pp" />
                                     </div>
                                     <div className="card__body">
-                                        <h4>App de Clima</h4>
-                                        <p>Proyecto para buscar clima en cualquier ciudad del mundo, 
-                                           consume una API para los datos. 
+                                        <h4>Street shoes</h4>
+                                        <p>Ecommerce for the purchase of sports shoes.
                                         </p>
-                                        {/* <a className="LinkWebSite" href="#" target="_blank">visitar</a> */}
+                                        <a className="LinkWebSite" href="http://drops.vercel.app/" target="_blank">Go</a>
                                     </div>
                                 </div>
                             </div>
@@ -142,9 +86,9 @@ function WebWorks() {
                     <div className="card" >
                         <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(7) : ''}>
                             <div className="card__face card__face--front">
-                                <h2>MOVIES APP</h2>
+                                <h2>CinemAPP</h2>
                                 <div className={ intFrameWidthWeb < 800 ? "tooltipTitle":"invisible"}>
-                                    da click para ver la información
+                                click to see the information
                                 </div>
                             </div>
                             <div className="card__face card__face--back" >
@@ -153,42 +97,96 @@ function WebWorks() {
                                         <img src={img9} alt="Trabajos Web" className="pp" />
                                     </div>
                                     <div className="card__body">
-                                        <h4>Películas y Series</h4>
-                                        <p>App para buscar películas y series y agregarlas a favoritos y 
-                                           conocer sus detalles. 
+                                        <h4>Cinema</h4>
+                                        <p>Ticket purchase app for online cinema.
                                         </p>
-                                        {/* <a className="LinkWebSite" href="#" target="_blank">visitar</a> */}
+                                         <a className="LinkWebSite" href="https://github.com/Tomasberro/Pf-henry-cinema/tree/main" target="_blank">Go</a> 
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div> 
                     {/* -------------------------------------------------------------------------- */}
-                    {/* -------------------------------------------------------------------------- */}
                     <div className="card" >
-                        <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(9) : ''}>
+                        <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(6) : ''}>
                             <div className="card__face card__face--front">
-                                <h2>DROPS APP</h2>
+                                <h2>Pokemon APP</h2>
                                 <div className={ intFrameWidthWeb < 800 ? "tooltipTitle":"invisible"}>
-                                    da click para ver la información
+                                click to see the information
                                 </div>
                             </div>
                             <div className="card__face card__face--back" >
                                 <div className="card__content" >
                                      <div className="card__header">
-                                        <img src={img12} alt="Trabajos Web" className="pp" />
+                                        <img src={img8} alt="Trabajos Web" className="pp" />
                                     </div>
                                     <div className="card__body">
-                                        <h4>Calzado de Moda</h4>
-                                        <p>Ecommerce para compra de calzado deportivo de moda, 
-                                           responsivo y con panel de administración. 
+                                        <h4>Pokemon desk</h4>
+                                        <p>Search pokemon, filter them / Sort them,
+                                create new pokemon
                                         </p>
-                                        {/* <a className="LinkWebSite" href="#" target="_blank">visitar</a> */}
+                                     <a className="LinkWebSite" href="https://pokemon-pi-tomib-7k182jxix-tomasberro.vercel.app/" target="_blank">Go</a> 
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div> 
+                    {/* -------------------------------------------------------------------------- */}
+                    <div className="card" >
+    <div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(4) : ''}>
+         <div className="card__face card__face--front">
+        <h2>Alkemy</h2>
+        <div className={ intFrameWidthWeb < 800 ? "tooltipTitle":"invisible"}>
+        click to see the information
+        </div>
+    </div>
+    <div className="card__face card__face--back" >
+        <div className="card__content" >
+             <div className="card__header">
+                {/* <img src={} alt="Trabajos Web" className="pp" /> */}
+            </div>
+            <div className="card__body">
+                <h4>Personal wallet</h4>
+                <p>Allows keeping accounts of income and personal
+            expenses of the domestic economy of a person 
+                </p>
+                <a className="LinkWebSite" href="https://github.com/Tomasberro/AlkemyTomas" target="_blank">Go</a> 
+            </div>
+        </div>
+    </div>
+</div>
+</div> 
+                    {/* -------------------------------------------------------------------------- */}
+                   
+<div className="card" >
+<div className="card__inner" onClick={ intFrameWidthWeb <= 800 ? ()=>flipped(3) : ''}>
+    <div className="card__face card__face--front">
+        <h2>Data science</h2>
+        <div className={ intFrameWidthWeb < 800 ? "tooltipTitle":"invisible"}>
+        click to see the information
+        </div>
+    </div>
+    <div className="card__face card__face--back" >
+        <div className="card__content" >
+             {/* <div className="card__header">
+                {/* <img src={img4} alt="Trabajos Web" className="pp" /> 
+            </div> */}
+            <div className="card__body">
+                <h4>K-Means-Clustering-London</h4>
+                <p>At work it's about helping to put security measures in place for
+                     banks and helping them predict what will be fakes banknotes. 
+                </p>
+                 <a className="LinkWebSite" href="https://github.com/Tomasberro/K-Means-Clustering-London" target="_blank">Go</a> 
+            </div>
+        </div>
+    </div>
+</div>
+</div> 
+                    {/* -------------------------------------------------------------------------- */}
+             
+                    {/* -------------------------------------------------------------------------- */}
+                    {/* -------------------------------------------------------------------------- */}
+              
                     {/* -------------------------------------------------------------------------- */}
 
                 </div>
@@ -199,3 +197,6 @@ function WebWorks() {
 }
 
 export default WebWorks
+
+
+
